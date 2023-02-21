b056153f3a6d44eec868388145b6b7a02ec86c66
fafaranca <franciszek@olechnowicz.pl>
2023-02-21 18:14:39 +0100

    Dodanie czcionek ikarty pikachu karta jest niedokonczona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatToolbarModule , } from '@angular/material/toolbar';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { MatCardModule , } from '@angular/material/card';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule],
+  declarations: [AppComponent, PokemonListComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatCardModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
